7c51640886c84d4c3bb6046d8379dc20574f2e62
Felipe Lippelt <lippelt.f@gmail.com>
2022-08-19 23:42:58 -0300

    feat: Login Page - Temporary solution for lint error

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 // import Header from '../components/Header';
-import profileIcon from '../images/profileIcon.svg';
+// import profileIcon from '../images/profileIcon.svg';
 // import Footer from '../components/Footer';
 
 function Profile() {
