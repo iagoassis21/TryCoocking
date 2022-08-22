1984e735bc0198cc185d2fa2aadc4e1981cf015f
celsorodrigo <celsorodrigoma@gmail.com>
2022-08-22 20:42:06 -0300

    fix: Header - Missing header - Completed\

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import RecipesFilterButtons from '../components/RecipesFilterButtons';
 import RecipeCard from '../components/RecipeCard';
 import Context from '../context/Context';
+import Header from '../components/Header';
 
 function Recipes() {
   const {
@@ -11,8 +12,11 @@ function Recipes() {
     displayRecipes,
   } = useContext(Context);
 
+  const pageTitle = () => pageType[0].toUpperCase() + pageType.substring(1);
+
   return (
     <div className="screen-size">
+      <Header title={ pageTitle() } />
       {mainLoading
         ? <h1>Loading...</h1>
         : <RecipesFilterButtons pageType={ pageType } />}
