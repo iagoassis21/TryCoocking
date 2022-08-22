4f41583b2a6368fc1e688d77605848c232b1044a
mateussousaa <mateussousa20@outlook.com>
2022-08-22 19:31:10 -0300

    feat: Req 7 ao 9 - Header - Completed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 
-// first commit
-
 function App() {
   return (
     <div className="screen-size">
