214dd900711192a6666c8a54c05d781cf965e7c5
iagoassis21 <iagoassis21@outlook.com>
2022-08-25 14:34:06 -0300

    feat: 37 ao 40 e 42  - Recipe In Progress - Not Completed

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,31 +1,84 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { drinkRecipeList, mealRecipeList } from '../helpers/fetchRecipeListApi';
+import '../App.css';
+import xincaraclabin from '../helpers/localStorageIngredients';
+
+const copy = require('clipboard-copy');
 
 function RecipeInProgress({ drink = false }) {
   const { id } = useParams();
   const [recipe, setRecipe] = useState([]);
+  const [ingredient, setCheckedIngredient] = useState([]);
+  const [shareRecipe, setShareRecipe] = useState('');
+  const history = useHistory();
+
   useEffect(() => {
     if (drink) {
-      const getRecipeList = async () => {
+      const getDrinkRecipeList = async () => {
         const results = await drinkRecipeList(id);
         setRecipe(results.drinks[0]);
       };
-
-      return getRecipeList();
+      getDrinkRecipeList();
+      if (!JSON.parse(localStorage.getItem('inProgressRecipes'))) {
+        return localStorage.setItem('inProgressRecipes',
+          JSON.stringify({ cocktails: {}, meals: {} }));
+      }
+      const getCheckedDrinks = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      if (getCheckedDrinks.cocktails[id]) {
+        setCheckedIngredient(getCheckedDrinks.cocktails[id]);
+      }
+      return;
     }
-    const getRecipeList = async () => {
+
+    const getMealRecipeList = async () => {
       const results = await mealRecipeList(id);
       setRecipe(results.meals[0]);
     };
-
-    getRecipeList();
+    getMealRecipeList();
+    if (!JSON.parse(localStorage.getItem('inProgressRecipes'))) {
+      return localStorage.setItem('inProgressRecipes',
+        JSON.stringify({ cocktails: {}, meals: {} }));
+    }
+    const getCheckedMeals = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (getCheckedMeals.meals[id]) {
+      setCheckedIngredient(getCheckedMeals.meals[id]);
+    }
   }, []);
 
+  const handleCheckIngredient = (e) => {
+    if (ingredient.includes(e.target.id)) {
+      const dullen = ingredient
+        .filter((markedIngredient) => markedIngredient !== e.target.id);
+      xincaraclabin(drink, { [id]: dullen });
+      return setCheckedIngredient(dullen);
+    }
+    setCheckedIngredient([...ingredient, e.target.id]);
+    const beckenjhonsons = { [id]: [...ingredient, e.target.id] };
+    xincaraclabin(drink, beckenjhonsons);
+  };
   const recipeIngredients = Object.keys(recipe || {})
     .filter((key) => key.includes('strIngredient') && (recipe[key]))
-    .map((item) => recipe[item]);
+    .map((item) => recipe[item]).map((validIngredient) => (
+      <label
+        htmlFor={ validIngredient }
+        key={ validIngredient }
+        className={ ingredient.includes(validIngredient) ? 'usedIngredient' : null }
+      >
+        <input
+          id={ validIngredient }
+          type="checkbox"
+          onChange={ (e) => handleCheckIngredient(e) }
+          checked={ ingredient.includes(validIngredient) }
+        />
+        {validIngredient}
+      </label>
+    ));
+
+  const clipBoardCopy = (
+    <span>Link copied!</span>
+  );
 
   const drinkRecipe = (
     <div>
@@ -39,25 +92,31 @@ function RecipeInProgress({ drink = false }) {
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ () => {
+          setShareRecipe(true);
+          copy(`http://localhost:3000/drinks/${id}`);
+        } }
       >
         Share Recipe
-
       </button>
+      {
+        shareRecipe ? clipBoardCopy : ''
+      }
       <button
         type="button"
         data-testid="favorite-btn"
       >
         Favorite Recipe
-
       </button>
       <p data-testid="recipe-category">{recipe.strCategory}</p>
       <p data-testid="instructions">{recipe.strInstructions}</p>
       <button
         type="button"
         data-testid="finish-recipe-btn"
+        disabled={ ingredient.length !== recipeIngredients.length }
+        onClick={ () => { history.push('/done-recipes'); } }
       >
         Finish Recipe
-
       </button>
     </div>
   );
@@ -73,9 +132,16 @@ function RecipeInProgress({ drink = false }) {
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ () => {
+          setShareRecipe(true);
+          copy(`http://localhost:3000/foods/${id}`);
+        } }
       >
         Share Recipe
       </button>
+      {
+        shareRecipe ? clipBoardCopy : ''
+      }
       <button
         type="button"
         data-testid="favorite-btn"
@@ -87,18 +153,19 @@ function RecipeInProgress({ drink = false }) {
       <button
         type="button"
         data-testid="finish-recipe-btn"
+        disabled={ ingredient.length !== recipeIngredients.length }
+        onClick={ () => { history.push('/done-recipes'); } }
       >
         Finish Recipe
-
       </button>
     </div>
   );
   return (
     <div>
-      {
-        drink ? mealRecipe : drinkRecipe
-      }
 
+      {
+        drink ? drinkRecipe : mealRecipe
+      }
       {
         recipeIngredients.map((ingredients, index) => (
           <p
